53901432204de2b5128cb5539543c4c44d2dd173
Maciej Wasiak <mwasiak154@gmail.com>
2020-08-28 18:00:04 +0200

    dateCheck

--- src/js/index.js
+++ src/js/index.js
@@ -11,26 +11,36 @@ const removeButton = document.querySelector('.button__remove--js');
 const glassQuantity = document.querySelector('.glass__quantity--js');
 
 let glassCounter;
+let today;
 
-if (localStorage.getItem('glassCounter') == null) {
-    localStorage.setItem('glassCounter', 0);
+function dateCheck (){
+    today = new Date();
+    today = `${today.getDate()}-${today.getMonth()+1}-${today.getFullYear()}`
+    console.log(today);
+    if (localStorage.getItem(today) == null) {
+        localStorage.setItem(today, 0);
+    }
+    glassCounter = parseFloat(localStorage.getItem(today));
+    glassQuantity.innerHTML = glassCounter;
 }
-glassCounter = parseFloat(localStorage.getItem('glassCounter'));
-glassQuantity.innerHTML = glassCounter;
+
+dateCheck()
 
 addButton.addEventListener('click', addGlass);
 removeButton.addEventListener('click', removeGlass);
 
 function addGlass() {
+    dateCheck();
     glassCounter++;
-    localStorage.setItem('glassCounter', glassCounter);
+    localStorage.setItem(today, glassCounter);
     glassQuantity.innerHTML = glassCounter;
 }
 
 function removeGlass() {
+    dateCheck();
     if (glassCounter > 0) {
         glassCounter--;
-        localStorage.setItem('glassCounter', glassCounter);
+        localStorage.setItem(today, glassCounter);
         glassQuantity.innerHTML = glassCounter;
     }
 }
