d0111e98e39798bd3fc2dfa7f81e617b905ba83b
Maciej Wasiak <mwasiak154@gmail.com>
2020-08-28 17:37:56 +0200

    local storage

--- src/js/index.js
+++ src/js/index.js
@@ -6,25 +6,31 @@ import '../scss/main.scss';
 
 /* place your code below */
 
-// console.log('HELLO 🚀')
-
-let glassCounter = 0;
-
 const addButton = document.querySelector('.button__add--js');
 const removeButton = document.querySelector('.button__remove--js');
-const glassQuantity = document.querySelector('.glass__quantity--js')
+const glassQuantity = document.querySelector('.glass__quantity--js');
+
+let glassCounter;
+
+if (localStorage.getItem('glassCounter') == null) {
+    localStorage.setItem('glassCounter', 0);
+}
+glassCounter = parseFloat(localStorage.getItem('glassCounter'));
+glassQuantity.innerHTML = glassCounter;
 
 addButton.addEventListener('click', addGlass);
 removeButton.addEventListener('click', removeGlass);
 
 function addGlass() {
     glassCounter++;
+    localStorage.setItem('glassCounter', glassCounter);
     glassQuantity.innerHTML = glassCounter;
 }
 
 function removeGlass() {
     if (glassCounter > 0) {
         glassCounter--;
+        localStorage.setItem('glassCounter', glassCounter);
         glassQuantity.innerHTML = glassCounter;
     }
 }
