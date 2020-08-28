29b7380ab7ac539cf9698e372c425d71dbd036ff
Maciej Wasiak <mwasiak154@gmail.com>
2020-08-28 17:26:03 +0200

    simple js

--- src/js/index.js
+++ src/js/index.js
@@ -6,4 +6,25 @@ import '../scss/main.scss';
 
 /* place your code below */
 
-console.log('HELLO 🚀')
+// console.log('HELLO 🚀')
+
+let glassCounter = 0;
+
+const addButton = document.querySelector('.button__add--js');
+const removeButton = document.querySelector('.button__remove--js');
+const glassQuantity = document.querySelector('.glass__quantity--js')
+
+addButton.addEventListener('click', addGlass);
+removeButton.addEventListener('click', removeGlass);
+
+function addGlass() {
+    glassCounter++;
+    glassQuantity.innerHTML = glassCounter;
+}
+
+function removeGlass() {
+    if (glassCounter > 0) {
+        glassCounter--;
+        glassQuantity.innerHTML = glassCounter;
+    }
+}
